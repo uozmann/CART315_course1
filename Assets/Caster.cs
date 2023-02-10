a26705b07f1ca393c44d993eebd5a98e382ddd6a
uozmann <1974155655@qq.com>
2023-02-10 12:29:59 -0500

    Class 5

---
+++ Assets/Caster.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caster : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray theRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHitInfo;
+
+                if (Physics.Raycast(theRay, out rayHitInfo))
+            {
+                GameObject.Instantiate(prefab, rayHitInfo.point, Quaternion.identity);
+            }
+        }
+
+    }
+}
